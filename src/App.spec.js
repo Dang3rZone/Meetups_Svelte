caa195590abe8f36d733da9fc07e5c5a06a52211
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-25 19:40:25 +0200

    Testing: added babel dependency that was missing  and created app test

---
+++ src/App.spec.js
@@ -0,0 +1,7 @@
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/svelte';
+
+import App from './App.svelte';
+
+describe('App', () => {
+  test('message and input have always same values', async () => {});
+});
