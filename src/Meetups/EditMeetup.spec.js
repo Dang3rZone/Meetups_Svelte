d90dfefa70abb1a5e3e3e9fe8d2ef65fc0f79d99
Andres Rosales <andresrosalespinzon@gmail.com>
2021-05-27 09:42:39 +0200

    test: checking save btn functionality (not working yet) and fixed some config files

---
+++ src/Meetups/EditMeetup.spec.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, fireEvent } from '@testing-library/svelte';
+import EditMeetup from './EditMeetup.svelte';
+
+const formData = {
+  id: null,
+  title: '',
+  subtitle: '',
+  address: '',
+  email: '',
+  description: '',
+  imageUrl: '',
+};
+
+describe('form validation', () => {
+  it.only('all inputs are required, save btn is disabled', async () => {
+    render(EditMeetup);
+    const saveBtn = document.querySelector("button[aria-label='SAVE']");
+    // btn is disabled by default
+
+    expect(saveBtn).toBeDisabled();
+    expect(saveBtn).toHaveAttribute('disabled');
+  });
+});
